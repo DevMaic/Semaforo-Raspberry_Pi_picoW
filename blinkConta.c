43630cbb90bc8b13e98012b4a36d1f950b84bfee
Maic <maic1405@gmail.com>
2025-05-05 02:08:05 -0300

    FEAT: matriz de leds adicionada ao projeto

--- blinkConta.c
+++ blinkConta.c
@@ -2,7 +2,9 @@
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "hardware/pwm.h"
+#include "hardware/pio.h"
 #include "hardware/clocks.h"
+#include "pio_matrix.pio.h"
 #include "lib/ssd1306.h"
 #include "lib/font.h"
 #include "FreeRTOS.h"
@@ -19,6 +21,53 @@ uint8_t activeLed = 1;
 bool buzzerActive = true; // Variável para controlar o estado do buzzer
 bool isNocturneModeOn = false; // Variável para controlar a alternância entre os modos 
 int buzzerFrequency = 2000; // Frequência do buzzer em Hz
+
+typedef struct {
+  double r;
+  double g;
+  double b;
+} Pixel;
+
+Pixel desenho[25] = { 
+    {0.0, 0.0, 0.0}, {0.01, 0.01, 0.01}, {0.01, 0.01, 0.01}, {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0},
+    {0.0, 0.0, 0.0}, {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0},    {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0},
+    {0.0, 0.0, 0.0}, {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0},    {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0},
+    {0.0, 0.0, 0.0}, {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0},    {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0},
+    {0.0, 0.0, 0.0}, {0.01, 0.01, 0.01}, {0.01, 0.01, 0.01}, {0.01, 0.01, 0.01}, {0.0, 0.0, 0.0}
+};
+
+//rotina para definição da intensidade de cores do led
+uint32_t matrix_rgb(double b, double r, double g)
+{
+  unsigned char R, G, B;
+  R = r * 255;
+  G = g * 255;
+  B = b * 255;
+  return (G << 24) | (R << 16) | (B << 8);
+}
+
+//rotina para acionar a matrix de leds - ws2812b
+void desenho_pio(PIO pio, uint sm) {
+    uint32_t valor_led;
+
+    for (int16_t i = 0; i < 25; i++) {
+        valor_led = matrix_rgb(
+        desenho[24-i].b,  // azul
+        desenho[24-i].r,  // vermelho
+        desenho[24-i].g   // verde
+        );
+        pio_sm_put_blocking(pio, sm, valor_led);
+    }
+}
+
+// Modifica a cor de píxels individuais na matriz desenho que representa a matriz de LEDs
+void set_pixel_color(int led_index, double r, double g, double b) {
+  if (led_index >= 0 && led_index < 25) {
+    desenho[led_index].r = r;
+    desenho[led_index].g = g;
+    desenho[led_index].b = b;
+  }
+}
 
 void vBlinkLedTask() {
     gpio_init(11);
@@ -38,6 +87,7 @@ void vBlinkLedTask() {
                 buzzerActive = true;
                 vTaskDelay(pdMS_TO_TICKS(1000));
                 buzzerActive = false;
+                vTaskDelay(pdMS_TO_TICKS(1000));
 
                 activeLed = 2; // Change to the next LED
             } else if(activeLed == 2) {
@@ -67,12 +117,10 @@ void vBlinkLedTask() {
                 buzzerActive = true;
                 vTaskDelay(pdMS_TO_TICKS(500));
                 buzzerActive = false;
-                vTaskDelay(pdMS_TO_TICKS(500));
+                vTaskDelay(pdMS_TO_TICKS(1500));
 
                 activeLed = 1; // Change to the next LED
             }
-
-            vTaskDelay(pdMS_TO_TICKS(1000)); // Delay para evitar uso excessivo da CPU
         } else {
             gpio_put(11, true);
             gpio_put(12, false);
@@ -109,7 +157,7 @@ void vBuzzerTask() {
     }
 }
 
-void vDisplay3Task() {
+void vDisplayTask() {
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400 * 1000);
 
@@ -130,19 +178,45 @@ void vDisplay3Task() {
     bool cor = true;
 
     while (true) {
-        sprintf(str_y, "%d", contador); // Converte em string
-        contador++;                     // Incrementa o contador
+        //  Atualiza o conteúdo do display com animações
         ssd1306_fill(&ssd, !cor);                          // Limpa o display
         ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
-        ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
-        ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
-        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
-        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
-        ssd1306_draw_string(&ssd, "  FreeRTOS", 10, 28); // Desenha uma string
-        ssd1306_draw_string(&ssd, "Contador  LEDs", 10, 41);    // Desenha uma string
-        ssd1306_draw_string(&ssd, str_y, 40, 52);          // Desenha uma string
-        ssd1306_send_data(&ssd);                           // Atualiza o display
-        sleep_ms(735);
+        ssd1306_line(&ssd, 20, 7, 45, 7, cor);           // Desenha uma linha
+        ssd1306_line(&ssd, 20, 7, 20, 57, cor);          // Desenha uma linha
+        ssd1306_line(&ssd, 20, 57, 45, 57, cor);           // Desenha uma linha
+        ssd1306_line(&ssd, 45, 7, 45, 57, cor);           // Desenha uma linha
+        vTaskDelay(pdMS_TO_TICKS(250)); // Delay para evitar uso excessivo da CPU
+
+        if(isNocturneModeOn) {
+            ssd1306_rect(&ssd, 12, 28, 11, 10, cor, !cor);      // Desenha um retângulo
+            ssd1306_rect(&ssd, 27, 28, 11, 10, cor, cor);      // Desenha um retângulo
+            ssd1306_rect(&ssd, 42, 28, 11, 10, cor, !cor);  
+        } else {
+            ssd1306_rect(&ssd, 12, 28, 11, 10, cor, activeLed==1?cor:!cor);      // Desenha um retângulo
+            ssd1306_rect(&ssd, 27, 28, 11, 10, cor, activeLed==2?cor:!cor);      // Desenha um retângulo
+            ssd1306_rect(&ssd, 42, 28, 11, 10, cor, activeLed==3?cor:!cor);      // Desenha um retângulo
+        }
+
+        ssd1306_draw_string(&ssd, activeLed==1?"Verde":activeLed==2?"Amarelo":"Vermelho", 50, 29);    // Desenha uma string
+        ssd1306_send_data(&ssd);    // Atualiza o display
+        vTaskDelay(1);
+    }
+}
+
+void vLedMatrixTask() {
+    //configurações da PIO
+    PIO pio = pio0;
+    set_sys_clock_khz(128000, false);
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, 7);
+
+    while(1) {
+        set_pixel_color(7, 0, activeLed==1?0.1:0, 0);
+        set_pixel_color(12, activeLed==2?0.1:0, activeLed==2?0.1:0, 0);
+        set_pixel_color(17, activeLed==3?0.1:0, 0, 0);
+        desenho_pio(pio, sm);
+        vTaskDelay(1); // Yield para outras tarefas
     }
 }
 
@@ -166,23 +240,24 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
 }
 
 int main() {
+    stdio_init_all();
+
     // Para ser utilizado o modo BOOTSEL com botão B
     gpio_init(botaoB);
     gpio_set_dir(botaoB, GPIO_IN);
     gpio_pull_up(botaoB);
     gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
-    // Botão A(5) usado para alternar entre os modos noturno e diurno
+    // Botão A usado para alternar entre os modos noturno e diurno
     gpio_init(botaoA);
     gpio_set_dir(botaoA, GPIO_IN);
     gpio_pull_up(botaoA);
     gpio_set_irq_enabled_with_callback(botaoA, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
-    stdio_init_all();
-
     xTaskCreate(vBlinkLedTask, "Blink Led Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
     xTaskCreate(vBuzzerTask, "Buzzer Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-    xTaskCreate(vDisplay3Task, "Cont Task Disp3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vDisplayTask, "Display Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL);
+    xTaskCreate(vLedMatrixTask, "LedMatrix Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL);
     vTaskStartScheduler();
     panic_unsupported();
 }
